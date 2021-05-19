569f825232536646c9d2918f04c5f75519eb067d
Hariprasath.R <balaprasad24@gmail.com>
2021-05-19 17:43:48 +0530

    Basic Blocks Example

--- src/test/groovy/in/hp/spock/basics/BlocksExampleSpec.groovy
+++ src/test/groovy/in/hp/spock/basics/BlocksExampleSpec.groovy
@@ -29,4 +29,31 @@ class BlocksExampleSpec extends Specification {
         and: "Should have the element on top"
         stack.peek() == element1
     }
+
+    def "Exception example"() {
+        when: "Popping an empty stack"
+        stack.pop()
+
+        then: "Should throw EmptyStackException"
+        // thrown(EmptyStackException)
+        // below is more clear and can make use of the exception
+        EmptyStackException e = thrown()
+        e.cause == null
+    }
+
+    def "Should not throw exception example"() {
+        given: "A HashMap"
+        def map = new HashMap<String, String>()
+
+        when: "Adding a null key to the HashMap"
+        map.put(null, 'value')
+
+        then: "Should not throw NullPointerException"
+        notThrown(NullPointerException)
+    }
+
+    def "Expect block example"() {
+        expect: "Expect 2 greater than 1"
+        Math.max(1, 2) == 2
+    }
 }
